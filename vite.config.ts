4dcc2c7ee46e7787e2b5bb15926b439613325786
Norrapat Nimmanee <charge_n@hotmail.com>
2026-08-15 13:53:24 +0700

    build: group output chunks by domain

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,30 @@ const manualHttpsConfig = fs.existsSync('./certs/server.key') && fs.existsSync('
     : undefined;
 export default defineConfig({
     plugins: [react(), ...(manualHttpsConfig ? [] : [basicSsl()])],
+    build: {
+        chunkSizeWarningLimit: 2048,
+        rolldownOptions: {
+            output: {
+                codeSplitting: {
+                    groups: [
+                        { name: 'react', test: /node_modules[\\/](react|react-dom|scheduler)/, priority: 3 },
+                        { name: 'three', test: /node_modules[\\/](three|three\.meshline)/, priority: 3 },
+                        { name: 'vendor', test: /node_modules/, priority: 2 },
+                        { name: 'engine', test: /src[\\/]engine/, priority: 2 },
+                        { name: 'game', test: /src[\\/]game/, priority: 2 },
+                        { name: 'network', test: /src[\\/]network/, priority: 2 },
+                        { name: 'data', test: /src[\\/]data/, priority: 2 },
+                        { name: 'menu', test: /src[\\/]gui[\\/]screen[\\/]mainMenu/, priority: 2 },
+                        { name: 'gameui', test: /src[\\/]gui[\\/]screen[\\/]game/, priority: 2 },
+                        { name: 'gui', test: /src[\\/]gui/, priority: 1 },
+                        { name: 'util', test: /src[\\/]util/, priority: 1 },
+                        { name: 'types', test: /src[\\/]types/, priority: 1 },
+                        { name: 'tools', test: /src[\\/]tools/, priority: 1 },
+                    ],
+                },
+            },
+        },
+    },
     server: {
         host: '0.0.0.0',
         port: devPort,
